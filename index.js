406e41fab43ef905f682babeab6f593ced82e2e9
LAPTOP-T21LK98P\KHS <https://github.com/Hosae-Coding>
2023-01-27 21:35:14 -0500

    created hotel route

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import authRoute from './api/routes/auth.js';
+import usersRoute from './api/routes/users.js';
+import hotelsRoute from './api/routes/hotels.js';
+import roomsRoute from './api/routes/rooms.js';
+
 const app = express();
 
 dotenv.config();
@@ -9,10 +14,28 @@ mongoose.set('strictQuery', false);
 const connect = async () => {
    try {
       await mongoose.connect(process.env.MONGO);
+      console.log('Success to conect to DB');
    } catch (error) {
       throw error;
    }
 };
+
+mongoose.connection.on('disconnected', () => {
+   console.log('DB disconnedted');
+});
+
+mongoose.connection.on('connected', () => {
+   console.log('DB connedted');
+});
+
+// middlewares
+
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
 
 app.listen(8800, () => {
    connect();
